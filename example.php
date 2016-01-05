4e90fe7518e0c99b80ee9a8fddb6648a87c651d4
Samy <samy.saad@gulfsip.com>
2016-01-05 14:20:13 +0300

    -Initial commit

---
+++ example.php
@@ -0,0 +1,26 @@
+<?php 
+include "macvendor_api.php";
+use \macvendors_co;
+
+$api = new \macvendors_co\MacVendorsApi();
+/**
+Get And parse data to array
+
+**/
+$vendor = $api->get_vendor ("08:74:02:00:00:00",'csv');
+echo $vendor['company'].'<br/>';
+echo $vendor['mac_prefix'].'<br/>';
+echo $vendor['address'];
+
+//Get raw data XML
+$vendor = $api->get_vendor_xml("08:74:02:00:00:00");
+echo '<pre>'.$vendor.'</pre>' ;
+//Get raw data CSV
+$vendor = $api->get_vendor_csv("08:74:02:00:00:00");
+echo '<pre>'.$vendor.'</pre>' ;
+//Get Raw data JSON
+$vendor = $api->get_vendor_json("08:74:02:00:00:00");
+echo '<pre>'.$vendor.'</pre>' ;
+//get raw data PIPE
+$vendor = $api->get_vendor_pipe("08:74:02:00:00:00");
+echo '<pre>'.$vendor.'</pre>' ;
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Macvendors.co API in PHP
+
+This class is the implementation of macvendors.co API.
+
+This class support alot of methods, please use the attached example.php file within this repository.
+
+For support please contact us via http://macvendors.co
